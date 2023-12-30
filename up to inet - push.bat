9e685377f0dac6390c8fb015ffbc6fd06ee348ce
stanislaumich <staslz42stas.com>
2023-12-30 22:01:28 +0300

    AUTO FROM HOME 30.12.2023 22:01:28,07

---
+++ up to inet - push.bat
@@ -0,0 +1,11 @@
+set /p place=< s:\place.txt
+cd BACKUP
+call _backupVCM.bat 
+cd ..
+git add .
+git commit -m "AUTO FROM %place% %date% %time%"
+git config --global http.version HTTP/1.1
+git push
+rem git push origin --force
+git config --global http.version HTTP/2
+pause
